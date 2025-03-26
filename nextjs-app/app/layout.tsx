9e1d75c57ad7346cdc979de22369ebb38111fb9e
yolaresofia <yolaresofia@gmail.com>
2025-03-26 10:58:42 +0100

    update animation + photos

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -1,36 +1,30 @@
+// app/layout.tsx
 import "./globals.css";
-
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { toPlainText } from "next-sanity";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
 import { handleError } from "./client-utils";
+import MotionLayout from "./components/MotionLayout";
 
-/**
- * Generate metadata for the page.
- * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
- */
 export async function generateMetadata(): Promise<Metadata> {
   const { data: settings } = await sanityFetch({
     query: settingsQuery,
-    // Metadata should never contain stega
     stega: false,
   });
   const title = settings?.title || "Alventosa Morell";
   const description = settings?.description;
-
   const ogImage = resolveOpenGraphImage(settings?.ogImage);
+
   let metadataBase: URL | undefined = undefined;
   try {
     metadataBase = settings?.ogImage?.metadataBase
       ? new URL(settings.ogImage.metadataBase)
       : undefined;
-  } catch {
-    // ignore
-  }
+  } catch {}
+
   return {
     metadataBase,
     title: {
@@ -44,28 +38,18 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
-});
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { data: settings } = await sanityFetch({
-    query: settingsQuery,
-  });
-
   return (
     <html lang="en">
       <body className="font-soehne">
-        <section>
-          <SanityLive onError={handleError} />
+        <SanityLive onError={handleError} />
+        <MotionLayout>
           <main>{children}</main>
-        </section>
+        </MotionLayout>
         <SpeedInsights />
       </body>
     </html>
